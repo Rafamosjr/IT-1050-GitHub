b393b12ef61609073099cb3c326e04b923ab781a
Rafael Moscoso <31458571+Rafamosjr@users.noreply.github.com>
2017-10-10 18:38:56 -0400

    LabAssignmentTwo

---
+++ Rmoscoso_LabAssignment2Solution/RmoscosoLabAssigment2/Person.cs
@@ -0,0 +1,26 @@
+﻿namespace RmoscosoLabAssigment2
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public Person p1Spouse;
+        public Person p2Spouse;
+        public string MaritalStatus;
+        public static double SumOfAllAges;
+
+        public string GetFullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+            public void PrintNameAndAge()
+        {
+            System.Console.WriteLine(this.FirstName + " " + this.Age);
+
+            {
+             
+            }
+        }
+    }
+}
---
+++ Rmoscoso_LabAssignment2Solution/RmoscosoLabAssigment2/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace RmoscosoLabAssigment2
+{
+    class Program
+    
+    {
+        static void Main(string[] args)
+        {
+            #region Person One
+            Person p1 = new Person();
+            System.Console.Write("Enter Your First Name: ");
+            p1.FirstName = System.Console.ReadLine();
+
+            System.Console.Write("Enter Your Last Name: ");
+            p1.LastName = System.Console.ReadLine();
+
+            System.Console.Write("Enter Your Age: ");
+            p1.Age = int.Parse(System.Console.ReadLine());
+
+            System.Console.Write("What is your marital status?: ");
+            p1.MaritalStatus = System.Console.ReadLine();
+                                                                            //I cannot seem to figure out why i keep getting System.NullReferenceException with this line.
+            System.Console.Write("What is your spouses first name?: ");
+            p1.p1Spouse.FirstName = System.Console.ReadLine();
+
+            System.Console.Write("What is your spouses age?: ");
+            p1.p1Spouse.Age = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine(p1.GetFullName());
+            System.Console.WriteLine(p1.p1Spouse.GetFullName());
+
+            p1.PrintNameAndAge();
+            p1.p1Spouse.PrintNameAndAge();
+
+            #endregion
+            #region Person Two
+            Person p2 = new Person();
+            System.Console.Write("Enter Your First Name: ");
+            p2.FirstName = System.Console.ReadLine();
+
+            System.Console.Write("Enter Your Last Name: ");
+            p2.LastName = System.Console.ReadLine();
+
+            System.Console.Write("Enter Your Age: ");
+            p2.Age = int.Parse(System.Console.ReadLine());
+
+            System.Console.Write("What is your marital status?: ");
+            p2.MaritalStatus = System.Console.ReadLine();
+
+            System.Console.Write("What is your spouses first name?: ");
+            p2.p2Spouse.FirstName = System.Console.ReadLine();
+
+            System.Console.Write("What is your spouses age?: ");
+            p2.p2Spouse.Age = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine(p2.GetFullName());
+            System.Console.WriteLine(p2.p2Spouse.GetFullName());
+
+            p2.PrintNameAndAge();
+            p2.p2Spouse.PrintNameAndAge();
+
+            #endregion
+
+
+            double SumOffAllAges = (p1.Age + p2.Age + p1.p1Spouse.Age + p2.p2Spouse.Age / 4);
+            System.Console.WriteLine(SumOffAllAges);
+
+
+        }
+    }
+}
